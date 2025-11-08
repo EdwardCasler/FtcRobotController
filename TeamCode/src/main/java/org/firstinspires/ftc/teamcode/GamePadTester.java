15624308d6bd62740c6cae7efb61bfd38a087338
EdwardCasler <edwardcasler@hotmail.com>
2025-11-08 12:25:08 -0800

    added gampadTester
    11/8/2025

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GamePadTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GamePadTester.java
@@ -2,24 +2,20 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 @TeleOp
-public class MotorTest extends LinearOpMode {
-    private DcMotor rightDrive;
+public class GamePadTester extends LinearOpMode {
 
     @Override
     public void runOpMode() {
-        rightDrive = hardwareMap.get(DcMotor.class, "rightDrive");
-
         waitForStart();
         if (opModeIsActive()) {
             while (opModeIsActive()) {
-                rightDrive.setPower(0.2);
+                telemetry.addData("Left Stick X", gamepad1.left_stick_x);
+                telemetry.addData("Left Stick Y", gamepad1.left_stick_y);
+                telemetry.update();
             }
         }
     }
 }
-//hi
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorTest2.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp
+public class MotorTest2 extends LinearOpMode {
+    private DcMotor rightDrive;
+    private DcMotor leftDrive;
+
+    @Override
+    public void runOpMode() {
+        rightDrive = hardwareMap.get(DcMotor.class, "rightDrive");
+        leftDrive = hardwareMap.get(DcMotor.class, "leftDrive");
+
+        waitForStart();
+        if (opModeIsActive()) {
+            while (opModeIsActive()) {
+                rightDrive.setPower(1);
+                leftDrive.setPower(-1);
+            }
+        }
+    }
+}
